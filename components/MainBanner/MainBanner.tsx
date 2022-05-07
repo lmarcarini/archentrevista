15908b572d9f38e6cd84d94f12c5c5e8a775e022
lmarcarini <leonardo.marcarini@yahoo.com.br>
2022-05-07 16:37:59 -0300

    main banner

--- components/MainBanner/MainBanner.tsx
+++ components/MainBanner/MainBanner.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import bannerImageDesktop from "../../public/images/banner/banner-desktop.jpg";
+import bannerImageMobile from "../../public/images/banner/banner-mobile.jpg";
+import styles from "./MainBanner.module.css";
 
 type Props = {};
 
 const MainBanner: React.FC = ({}: Props) => {
-  return <div>MainBanner</div>;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const isMobile = window.innerWidth < 768;
+    setIsMobile(isMobile);
+    console.log(isMobile);
+  }, []);
+
+  return (
+    <div className={styles.bannerContainer}>
+      <div className={styles.imageDesktop}>
+        <Image
+          src={bannerImageDesktop}
+          alt="banner"
+          layout={isMobile ? "intrinsic" : "fixed"}
+          height={450}
+          width={1920}
+          placeholder="blur"
+        />
+      </div>
+      <div className={styles.imageMobile}>
+        <Image
+          src={bannerImageMobile}
+          alt="banner"
+          layout="intrinsic"
+          height={454}
+          width={545}
+          placeholder="blur"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MainBanner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.main}>
       <MainBanner />
     </div>
   );
